b166957f32a735531fa7096fac77f4e6799bd271
Greg Caporaso <greg.caporaso@nau.edu>
2025-04-15 13:45:23 -0700

    squash

--- docs/book/q2doc-plug.py
+++ docs/book/q2doc-plug.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+# ----------------------------------------------------------------------------
+# Copyright (c) 2024, Caporaso Lab (https://cap-lab.bio).
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
 
 from q2doc.__main__ import myst
 
